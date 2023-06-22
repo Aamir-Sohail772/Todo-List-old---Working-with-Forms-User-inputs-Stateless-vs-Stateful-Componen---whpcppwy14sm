c2527ce14831a7df1d44012caaee7691dae0bfd3
Aamir Sohail <80810119+Aamir-Sohail772@users.noreply.github.com>
2023-06-22 21:03:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -66,58 +66,6 @@ export default App;
 
 
 
-// import React, { useState } from "react";
-// import "./../styles/App.css";
-
-// function App() {
-//   const [task, setTask] = useState("");
-//   const [tasks, setTasks] = useState([]);
-
-//   const handleTaskChange = (e) => {
-//     setTask(e.target.value);
-//   };
-
-//   const addTask = () => {
-//     if (task.trim() !== "") {
-//       setTasks([...tasks, task]);
-//       setTask("");
-//     }
-//   };
-
-//   const deleteTask = (index) => {
-//     const updatedTasks = [...tasks];
-//     updatedTasks.splice(index, 1);
-//     setTasks(updatedTasks);
-//   };
-
-//   const editTask = (index) => {
-//     const updatedTasks = [...tasks];
-//     updatedTasks[index] = task;
-//     setTasks(updatedTasks);
-//   };
-
-//   return (
-//     <div id="main">
-//       <textarea id="task" onChange={handleTaskChange} value={task}></textarea>
-//       <button id="btn" onClick={addTask}>Add Task</button>
-
-//       <ul>
-//         {tasks.map((task, index) => (
-//           <li key={index} className="list">
-//             {task}
-//             <button className="edit" onClick={() => editTask(index)}>Edit</button>
-//             <button className="delete" onClick={() => deleteTask(index)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 
 
 // import React from "react";
