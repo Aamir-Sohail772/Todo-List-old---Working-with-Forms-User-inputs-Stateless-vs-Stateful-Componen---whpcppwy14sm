2fceb41aee82cf731c8b21082bf19d725835b22b
Aamir Sohail <80810119+Aamir-Sohail772@users.noreply.github.com>
2023-06-22 21:00:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -22,9 +22,9 @@ function App() {
     setTasks(updatedTasks);
   };
 
-  const editTask = (index) => {
+  const editTask = (index, editedTask) => {
     const updatedTasks = [...tasks];
-    updatedTasks[index] = task;
+    updatedTasks[index] = editedTask;
     setTasks(updatedTasks);
   };
 
@@ -37,8 +37,20 @@ function App() {
         {tasks.map((task, index) => (
           <li key={index} className="list">
             {task}
-            <button className="edit" onClick={() => editTask(index)}>Edit</button>
+            <button className="edit" onClick={() => editTask(index, task)}>Edit</button>
             <button className="delete" onClick={() => deleteTask(index)}>Delete</button>
+            <textarea
+              className="editTask"
+              onChange={(e) => editTask(index, e.target.value)}
+              value={task}
+            ></textarea>
+            <button
+              className="saveTask"
+              onClick={() => editTask(index, task)}
+              disabled={task.trim() === ""}
+            >
+              Save
+            </button>
           </li>
         ))}
       </ul>
@@ -47,6 +59,62 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import "./../styles/App.css";
+
+// function App() {
+//   const [task, setTask] = useState("");
+//   const [tasks, setTasks] = useState([]);
+
+//   const handleTaskChange = (e) => {
+//     setTask(e.target.value);
+//   };
+
+//   const addTask = () => {
+//     if (task.trim() !== "") {
+//       setTasks([...tasks, task]);
+//       setTask("");
+//     }
+//   };
+
+//   const deleteTask = (index) => {
+//     const updatedTasks = [...tasks];
+//     updatedTasks.splice(index, 1);
+//     setTasks(updatedTasks);
+//   };
+
+//   const editTask = (index) => {
+//     const updatedTasks = [...tasks];
+//     updatedTasks[index] = task;
+//     setTasks(updatedTasks);
+//   };
+
+//   return (
+//     <div id="main">
+//       <textarea id="task" onChange={handleTaskChange} value={task}></textarea>
+//       <button id="btn" onClick={addTask}>Add Task</button>
+
+//       <ul>
+//         {tasks.map((task, index) => (
+//           <li key={index} className="list">
+//             {task}
+//             <button className="edit" onClick={() => editTask(index)}>Edit</button>
+//             <button className="delete" onClick={() => deleteTask(index)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
